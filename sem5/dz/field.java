e36ed2bad95ddc66a68a6d0d4a4554b913fc4f9b
[am_6733] <[anton6733@gmail.com]>
2023-03-01 16:21:13 +0200

    tree ready :( :)

--- sem5/dz/field.java
+++ sem5/dz/field.java
@@ -2,9 +2,14 @@
  * рисуем карту по волновому алгоритму;
  */
 
+import java.security.KeyPair;
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.sound.midi.Soundbank;
 
 public class field {
     
@@ -20,13 +25,17 @@ public class field {
     private final Integer emptyCell = -2;
     private final Integer startValue = 0;
     private final Integer destinationValue = -1;
+
+    private Hashtable<String,Integer> map_ = new Hashtable<>(){{}};
     
     public static void main(String[] args) {
+        
         field yep = new field(12,12);
+        yep.showLegend();
         yep.setWalls();
-        yep.ShowDesk("дефолтная доска");
+        // yep.ShowDesk("дефолтная доска");
         yep.SetRoutePoints();
-        yep.ShowDesk("Доска с финишем");
+        // yep.ShowDesk("Доска с финишем:");
         yep.drawRoute();
         System.out.println("");
     }
@@ -62,7 +71,7 @@ public class field {
             "Ставим стены, в формате \"x,y\".",
             "Начало отсчёта - левый верхний угол.",
             String.format("\nx принадлежит [1,%d], y принадлежит [1,%d].",
-            this.width-1,this.height-1),
+            this.width-2,this.height-2),
             "Неадекватный ввод для завершения."
         });
         System.out.println(inputPrompt);
@@ -119,10 +128,10 @@ public class field {
         Matcher ParseIt;
         String prompt_start = String.format(
             "%s в формате \"x,y\", x принадлежит [1,%d], y принадлежит [1,%d]",
-            "Отправная точка",this.width-1,this.height-1);
+            "Отправная точка",this.width-2,this.height-2);
         String prompt_fin = String.format(
             "%s в формате \"x,y\", x принадлежит [1,%d], y принадлежит [1,%d]",
-            "Конечная точка",this.width-1,this.height-1);
+            "Конечная точка",this.width-2,this.height-2);
         boolean correct = false;
         while (!correct){
             System.out.println(prompt_start);//1
@@ -185,68 +194,105 @@ public class field {
     }
 
     void drawRoute(){
+        Integer tesst = 0;
         waveNode[][] grid = new waveNode[this.width][this.height];
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
-                grid[i][j] = new waveNode(body[i][j]);
+            for (int j = 0; j < grid[i].length; j++) {
+                // grid[i][j] = new waveNode(body[i][j]);
+                grid[i][j] = new waveNode(tesst);
+                // System.out.print(tesst);
+                // System.out.print(" ");
+                tesst++;
             }
+            // System.out.println();
         }
-        if ((width <2) || (height <2)) { 
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                System.out.print(grid[i][j].value);
+            System.out.print(" ");            }
+            System.out.println();
+        }
+        if ((width <3) || (height <3)) { 
             return;} // потом об этом подумаю
         //знакомим соседей по углам
         //  сз
-        grid[0][0].bottomNeighboor = grid[1][0];
-        grid[0][0].rightNeghboor = grid[0][1];
-        
+        grid[1][1].bottomNeighboor = grid[2][1];
+        grid[1][1].rightNeighboor = grid[1][2];
+
         //  юз
-        grid[0][this.height-1].upperNeigboor = grid[0][this.height-2];
-        grid[0][this.height-1].rightNeghboor = grid[1][this.height-1];
+        grid[this.height-2][1].upperNeighboor = grid[this.height-3][1];
+        grid[this.height-2][1].rightNeighboor = grid[this.height-2][2];
 
         //  св
-        grid[this.width-1][0].leftNeigboor = grid[this.width-2][0];
-        grid[this.width-1][0].bottomNeighboor = grid[this.width-1][1];
+        grid[1][this.width-2].leftNeighboor = grid[1][this.width-3];
+        grid[1][this.width-2].bottomNeighboor = grid[2][this.width-2];
 
         // юв
-        grid[this.width-1][this.height-1].upperNeigboor= grid[this.width-1][this.height-2];
-        grid[this.width-1][this.height-1].leftNeigboor= grid[this.width-2][this.height-1];
+        grid[this.width-2][this.height-2].upperNeighboor
+        = grid[this.width-3][this.height-2];
+        grid[this.width-2][this.height-2].leftNeighboor
+        = grid[this.width-2][this.height-3];
         //знакомим соседей по краям
 
         //  верхний край
-        for (int i = 1; i < this.width-1; i++) {
+        for (int i = 2; i < this.width-2; i++) {
             grid[1][i].bottomNeighboor = grid[2][i];
-            grid[1][i].leftNeigboor =grid[1][i-1];
-            grid[1][i].rightNeghboor =grid[1][i+1];
+            grid[1][i].leftNeighboor =grid[1][i-1];
+            grid[1][i].rightNeighboor =grid[1][i+1];
         }
         //  нижний край
-        for (int i = 1; i < this.width-1; i++) {
-            grid[this.height-2][i].upperNeigboor = grid[this.height-1][i];
-            grid[this.height-2][i].leftNeigboor =grid[this.height-1][i-1];
-            grid[this.height-2][i].rightNeghboor =grid[this.height-1][i+1];
+        for (int i = 2; i < this.height-2; i++) {
+            grid[this.height-2][i].upperNeighboor = grid[this.height-3][i];
+            grid[this.height-2][i].leftNeighboor =grid[this.height-2][i-1];
+            grid[this.height-2][i].rightNeighboor =grid[this.height-2][i+1];
         }
         //  правый край
-        for (int i = 1; i < grid.length-1; i++) {
-            grid[this.width-2][i].upperNeigboor = grid[this.width-2][i-1];
-            grid[this.width-2][i].leftNeigboor = grid[this.width-3][i];
-            grid[this.width-2][i].bottomNeighboor = grid[this.width-2][i+1];
+        for (int i = 2; i < this.height-2; i++) {
+            grid[i][this.width -2].upperNeighboor = grid[i][this.width -1];
+            grid[i][this.width -2].leftNeighboor = grid[i][this.width -3];
+            grid[i][this.width -2].bottomNeighboor = grid[i][this.width -1];
         }
         //  левый край
-        for (int i = 1; i < grid.length-1; i++) {
-            grid[1][i].upperNeigboor = grid[1][i-1];
-            grid[1][i].rightNeghboor = grid[2][i];
-            grid[1][i].bottomNeighboor = grid[1][i+1];}
-
-        //знакомим мид
-        for (int i = 2; i < this.width-2; i++) {
-            for (int j = 2; j < this.height-2; j++) {
-                grid[i][j].upperNeigboor=grid[i][j-1];
-                grid[i][j].bottomNeighboor=grid[i][j+1];
-                grid[i][j].leftNeigboor=grid[i-1][j];
-                grid[i][j].rightNeghboor=grid[i+1][j];
+        for (int i = 2; i < grid.length-2; i++) {
+            grid[i][1].upperNeighboor = grid[i-1][1];
+            grid[i][1].rightNeighboor = grid[i][2];
+            grid[i][1].bottomNeighboor = grid[i+1][1];}
+//08/05/ dz stas
+        // знакомим мид
+        for (int i = 3; i < this.width-3; i++) {
+            for (int j = 3; j < this.height-3; j++) {
+                grid[i][j].upperNeighboor=grid[i-1][j];
+                grid[i][j].bottomNeighboor=grid[i+1][j];
+                grid[i][j].leftNeighboor=grid[i][j-1];
+                grid[i][j].rightNeighboor=grid[i][j+1];
             }
         }
-
+        System.out.println("вк");
+        waveNode[] test = grid[1][2].getNeigboors();
+        // System.out.println("нк");
+        // test = grid[1][2].getNeigboors();
+        System.out.println("нк");
+        test = grid[grid.length-2][2].getNeigboors();
+        System.out.println("пк");
+        test = grid[1][grid[0].length-3].getNeigboors();
+        System.out.println("лк");
+        test = grid[2][1].getNeigboors();
+        System.out.println("мид");
+        test = grid[5][5].getNeigboors();
+        
         System.out.println();
 
+    }
+    public void showLegend(){
+        this.map_.put("Стены:",this.border);
+        this.map_.put("Финиш:",this.destinationValue);
+        this.map_.put("Старт", this.startValue);
+        this.map_.put("Вакантное", this.emptyCell);
+        for ( var item : this.map_.entrySet()) {
+            System.out.println(
+                String.format("%s: %d", item.getKey(),
+                item.getValue()));
+        }
     }
 
 }
--- sem5/dz/waveNode.java
+++ sem5/dz/waveNode.java
@@ -3,9 +3,9 @@
  */
 public class waveNode {
     waveNode parent;
-    waveNode leftNeigboor;
-    waveNode rightNeghboor;
-    waveNode upperNeigboor;
+    waveNode leftNeighboor;
+    waveNode rightNeighboor;
+    waveNode upperNeighboor;
     waveNode bottomNeighboor;
 
     Integer value;
@@ -17,5 +17,30 @@ public class waveNode {
 
     public waveNode(Integer value_i){
         this.value= value_i;
+    }
+    public  waveNode[] getNeigboors(){
+        System.out.println(String.format("this.value = %d", this.value));
+        waveNode[] out = new waveNode[]{null,null,null,null};
+        if (this.leftNeighboor!= null){
+            System.out.println(String.format("left value: %d",
+            this.leftNeighboor.value));
+            out[0] = this.leftNeighboor;
+        }
+        if (this.rightNeighboor!= null){
+            System.out.println(String.format("right value: %d",
+            this.rightNeighboor.value));
+            out[1] = this.rightNeighboor;
+        }
+        if (this.upperNeighboor!= null){
+            System.out.println(String.format("upper value: %d",
+            this.upperNeighboor.value));
+            out[2] = this.upperNeighboor;
+        }
+        if (this.bottomNeighboor!= null){
+            System.out.println(String.format("lower value: %d",
+            this.bottomNeighboor.value));
+            out[3] = this.bottomNeighboor;
+        }
+        return out; 
     }
 }
