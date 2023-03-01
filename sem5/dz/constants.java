953f1e888dcebf3ec78683f4ef6dc0a4a32e3d11
am_6733 <anton6733@gmail.com>
2023-03-01 22:35:12 +0200

    const

---
+++ sem5/dz/constants.java
@@ -0,0 +1,19 @@
+/**
+ * Side
+ */
+public class constants {
+    public static String left = "left";
+    public static String right = "right";
+    public static String bottom = "bottom";
+    public static String top = "top";
+
+
+    public static final Integer cellWidth = 5;
+    public static final Integer border = -3;
+    public static  final Integer emptyCell = -2;
+    public static final Integer startValue = 0;
+    public static final Integer destinationValue = -1;
+    public static void main(String[] args) {
+        
+    }
+}
